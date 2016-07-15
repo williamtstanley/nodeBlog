6c49e2450572e74340181c6552c0f2dd21d2a0a7
William Stanley <william.t.stanley@gmail.com>
2016-07-15 10:17:56 -0700

    Initial commit - Basic Blog post CRUD

---
+++ models/blog.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var BlogSchema = new Schema({
+  title: {type: String, required: true},
+  body: {type: String, required: true},
+  created_at: Date,
+  updated_at: Date
+});
+
+// on every save, add the date
+BlogSchema.pre('save', function(next) {
+  // get the current date
+  var currentDate = new Date();
+  
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at doesn't exist, add to that field
+  if (!this.created_at)
+    this.created_at = currentDate;
+
+  next();
+});
+
+// BlogSchema.virtual('created_at.formated').get(function () {
+//   return this.created_at.toString().replace(/\S+\s(\S+)\s(\d+)\s(\d+)\s.*/,'$2-$1-$3');
+// });
+BlogSchema.methods.formatedDate = function () {
+  return this.created_at.toString().replace(/\S+\s(\S+)\s(\d+)\s(\d+)\s.*/,'$2-$1-$3');
+}
+
+var Blog = mongoose.model("Blog", BlogSchema);
+
+module.exports = Blog;
+
+
---
+++ routes/blogs.js
@@ -0,0 +1,90 @@
+var express = require("express");
+var router = express.Router();
+var Blog = require("../models/blog");
+
+//code goes here
+// Index
+router.get("/", function(request, response, next){
+  Blog.find(function(err, blogs){
+    if(err){
+      err.status = 404;
+      next(err, response, request);
+    }else{
+      response.render("blogs/index", {blogs: blogs});
+    }
+  });
+});
+
+// New
+router.get("/new", function(request, response){
+  response.render("blogs/new", {errors: {}, blog:{}} );
+});
+
+// Create
+router.post("/", function(request, response){
+  var params = request.body;
+  var blog = new Blog({title: params.title, body: params.body});
+  blog.save(function(err, blog){
+    if(err){
+      console.log(err);
+      response.render("blogs/new", {errors: err.errors, blog: params});
+    }else{
+      response.redirect("/blogs/" + blog._id);
+    }
+  });
+});
+
+//Show
+router.get("/:id", function(request, response, next){
+  Blog.findOne({_id: request.params.id}, function(err, blog){
+    if(err){
+      err.status = 404;
+      next(err, request, response)
+    } else {
+      response.render("blogs/show", {blog: blog});
+    }
+  });
+});
+//Edit Page
+router.get("/:id/edit", function(request, response){
+  Blog.findOne({_id: request.params.id}, function(err, blog){
+    if(err) {
+      response.render('error', {message: "Blog not found",
+                           error: {status: 404}});
+    } else {
+      response.render("blogs/edit", {blog: blog, errors: {}});
+    }
+  });
+});
+//Update
+router.patch("/:id", function(request, response){
+  Blog.findOne({_id: request.params.id}, function(err, blog){
+    var params = request.body
+    if(err) {
+      response.render('error', {message: "Blog not found",
+                           error: {status: 404}});
+    } else {
+      blog.title = params.title;
+      blog.body  = params.body;
+      blog.save(function(err, blog){
+        if(err) {
+          response.render("blogs/edit", {errors: err.errors, blog: blog});
+        } else {
+          response.redirect("/blogs/" + blog._id);
+        }
+      });
+    }
+  });
+});
+//Delete
+router.delete("/:id", function(request, response){
+  Blog.remove({_id:request.params.id}, function(err, blog){
+    if(err){
+      response.render("blogs/" + blog.id, {errors: err.errors, blog: blog});
+    } else {
+      response.redirect("/blogs");
+    }
+  });
+});
+
+module.exports = router;
